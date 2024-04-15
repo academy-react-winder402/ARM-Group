6b9fdf5e9ccc0cde38aca1da28cfdfe1084b64c0
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-14 19:20:48 -0700

    first commit in courses

--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,7 @@ import Courses from "./Pages/Courses/Courses.jsx";
 const router = new createBrowserRouter([
   { path: "/LogIn", element: <LogIn /> },
   { path: "/Courses", element: <Courses /> },
+  { path: "*", element: <h1>Page not found!</h1> },
 ]);
 
 export default router;
