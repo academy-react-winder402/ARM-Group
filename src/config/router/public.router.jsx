3000bd2f61314ae9eeb8254382c4c105932c0cea
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-04-21 20:57:27 +0330

    Header Beta added

--- src/config/router/public.router.jsx
+++ src/config/router/public.router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import LogIn from "../../Pages/LogIn/LogIn.jsx";
 import Courses from "../../Pages/Courses/Courses.jsx";
 import Landing from "../../Pages/Landing/Landing.jsx";
+import Header from "../../Components/MainHeader/MainHeader.jsx";
 
 const Public = new createBrowserRouter([
   { path: "/Landing", element: <Landing /> },
   { path: "/LogIn", element: <LogIn /> },
   { path: "/Courses", element: <Courses /> },
+  { path: "/Header", element: <Header /> },
 ]);
 
 export default Public;
