32ef9072443ef02d9cdd0d5e93993be2951feb27
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-24 16:49:28 -0700

    section-1 coppy in main

--- src/Components/Landing/Section_1/Container.jsx
+++ src/Components/Landing/Section_1/Container.jsx
@@ -1,7 +1,17 @@
 function Section_1() {
   return (
-    <div className="border border-black w-[1300px] h-[458px] m-auto mt-[30px] text-[30px] text-center">
-      Section 1
+    <div
+      className="w-[1300px] h-[458px] m-auto mt-[30px] text-[30px] text-right flex flex-col justify-start"
+      style={{ direction: "rtl" }}
+    >
+      <h1 className="text-3x text-textColor mt-16">آکادمی بحرالعلوم</h1>
+      <p className="inline-block text-[#7e7e7e] text-base w-[560px] leading-9 my-12">
+        آکادمی جامعه کوچکی است از ذهن های بزرگ برای رشد مراتب تخصص و دانستگی هدف
+        تغییر سرنوشت هاست با درک دوباره لذت کدنویسی
+      </p>
+      <button className="text-base text-center bg-[#E1C564] w-[180px] h-[40px] rounded-lg text-white shadow-lg hover:text-[#343a40] hover:bg-[#339af0]">
+        مشاهده دوره ها
+      </button>
     </div>
   );
 }
