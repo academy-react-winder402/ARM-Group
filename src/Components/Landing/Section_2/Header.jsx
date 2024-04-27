bf1af526587ce234d899cbd5e23368a3f040c700
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-27 10:41:51 -0700

    changes section 2

---
+++ src/Components/Landing/Section_2/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <>
+      <h2 className="text-3xl text-textColor pt-12">خدمات پژوهشگاه</h2>
+      <p className="text-base mt-8 text-textPcolor">
+        همه آنچه برای طی کردن مسیر پیشرفت نیاز دارید
+      </p>
+    </>
+  );
+};
