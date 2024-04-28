57ab6c11308cb8adda157e8754a9928bb856b87c
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-28 12:12:40 -0700

    continue the last title commit

--- src/Components/Landing/LandingHandler.jsx
+++ src/Components/Landing/LandingHandler.jsx
@@ -3,7 +3,7 @@ import SvgBackGround from "./SvgBackGround.jsx";
 import { Section_1 } from "./Section_1/Container.jsx";
 import { Section_2 } from "./Section_2/Container.jsx";
 import { Section_3 } from "./Section_3/Container.jsx";
-// import { MainNews } from "./NewsAndArticles/MainNews.jsx";
+import { MainNews } from "./NewsAndArticles/MainNews.jsx";
 
 function LandingHandler() {
   return (
@@ -13,7 +13,7 @@ function LandingHandler() {
       <Section_1 />
       <Section_2 />
       <Section_3 />
-      {/* <MainNews /> */}
+      <MainNews />
     </>
   );
 }
