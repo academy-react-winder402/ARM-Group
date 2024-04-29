0abb2ea8974fda4fddd4d0d13be000511ffd63b5
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-29 15:40:45 -0700

    course landing image

--- src/Components/Landing/Course/Container.jsx
+++ src/Components/Landing/Course/Container.jsx
@@ -1,14 +1,21 @@
 export const Course = () => {
   return (
     <div
-      className="border cotainer md:w-[1300px] h-[800px] m-auto mt-72 text-right mb-10"
+      className="cotainer md:w-[1300px] h-[800px] m-auto mt-72 text-right mb-10"
       dir="rtl"
     >
       <div className="text-center mx-auto w-1/2">
         <h2 className="text-textColor font-bold text-3xl">
           دوره های آموزشی ما
         </h2>
-        <p className="mt-8 text-textPcolor">متن مرتبط</p>
+        <p className="mt-16 text-textPcolor">متن مرتبط</p>
+      </div>
+      <div className="w-4/5 mx-auto relative">
+        <img
+          className="mt-20 absolute right-10"
+          src="./Image/Landing/coursesGroup.png"
+          alt=""
+        />
       </div>
     </div>
   );
--- src/Components/Landing/SvgBackGround.jsx
+++ src/Components/Landing/SvgBackGround.jsx
@@ -1,7 +1,7 @@
 export const SvgBackGround = () => {
   return (
     <img
-      className="w-[1300px] m-auto absolute z-[-1] left-[150px]"
+      className="w-[1300px] m-auto absolute z-[-1] left-[125px]"
       src="./Image/BackGound/backGround.svg"
       alt="Main BackGround"
     />
