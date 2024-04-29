51358ee588e557d8f8e72cf9a2e03e4f84c06b09
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-04-28 19:26:28 -0700

    Fix names

--- src/Components/Landing/LandingHandler.jsx
+++ src/Components/Landing/LandingHandler.jsx
@@ -2,7 +2,7 @@ import Svg from "./Svg";
 import SvgBackGround from "./SvgBackGround.jsx";
 import { HeroSection } from "./HeroSection//Container.jsx";
 import { Product } from "./Product/Container.jsx";
-import { Education } from "./Section_3/Container.jsx";
+import { Education } from "./Education/Container.jsx";
 import { MainNews } from "./NewsAndArticles/MainNews.jsx";
 
 function LandingHandler() {
@@ -17,5 +17,6 @@ function LandingHandler() {
     </>
   );
 }
+``;
 
 export default LandingHandler;
