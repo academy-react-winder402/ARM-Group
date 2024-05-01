2cccc717aee1865a54498a51345f645bdd77af4a
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-01 16:45:51 +0330

    DarkColors Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ export default {
         textPaleColor: "#9A9A9A",
         textMidColor: "#8A8A8A",
         backGroundColor: "#e9ecef",
+
+        /* Dark Mode Colors */
+        Dark_TextColor_1: "rgb(173, 165, 155)",
+        Dark_TextColor_2: "#989082",
+        Dark_ButtonColor: "#665314",
+        Dark_TextHoverColor: "#D8B649",
+        Dark_MainBG: "#1E2022",
+        Dark_ContainerBG: "#1A1C1E",
+        Dark_InputBG_1: "#121212",
       },
       boxShadow: {
         "3xl": "1px 1px 6px 0px #adb5bd",
