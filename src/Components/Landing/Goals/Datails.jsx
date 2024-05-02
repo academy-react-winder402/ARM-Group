160463a152fa5ebc190da49bbba959cdf7a84da1
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-05-01 22:42:02 -0700

    Dark Mode LandingPage

--- src/Components/Landing/Goals/Datails.jsx
+++ src/Components/Landing/Goals/Datails.jsx
@@ -16,12 +16,12 @@ export const Deatails = () => {
             src="./Image/Landing/Goals01.svg"
             alt="استعداد یابی"
           />
-          <h3 className="mt-16 mr-4 text-xl font-light text-textColor">
+          <h3 className="mt-16 mr-4 text-xl font-light text-textColor dark:text-Dark_TextColor_1">
             استعداد یابی
           </h3>
         </div>
         <div className="h-36 flex gap-56 pt-2">
-          <span className="w-1/2 h-16 text-textColor leading-10">
+          <span className="w-1/2 h-16 text-textColor leading-10 dark:text-Dark_TextColor_2">
             یافتن رگه های علاقه و استعداد در دوره های پایلوت استعدادیابی و سپس
             چینش مسیری برای شکوفا نمودن ذوق برنامه سازی در دانش پژوهان
           </span>
@@ -31,7 +31,7 @@ export const Deatails = () => {
               src="./Image/Landing/Goals02.svg"
               alt="راهنمایی و ایجاد انگیزه"
             />
-            <p className="mt-16 mr-4 text-xl font-light text-textColor">
+            <p className="mt-16 mr-4 text-xl font-light text-textColor dark:text-Dark_TextColor_1">
               راهنمایی و ایجاد انگیزه
             </p>
           </div>
@@ -43,17 +43,17 @@ export const Deatails = () => {
               src="./Image/Landing/Goals03.svg"
               alt="آموزش های تخصصی"
             />
-            <h3 className="mt-16 mr-4 text-xl font-light text-textColor">
+            <h3 className="mt-16 mr-4 text-xl font-light text-textColor dark:text-Dark_TextColor_1">
               آموزش های تخصصی
             </h3>
           </div>
-          <p className="w-1/2 h-16 text-textColor leading-10">
+          <p className="w-1/2 h-16 text-textColor leading-10 dark:text-Dark_TextColor_2">
             آشنایی با پشته ای تکنولوژیک از زبان های کدنویسی با نگاهی عمل محور
             برای تحریک ذهنیت خلاق در طول فرآیند آموزش
           </p>
         </div>
         <div className="h-36 flex gap-56">
-          <p className="w-1/2 h-16 text-textColor leading-10">
+          <p className="w-1/2 h-16 text-textColor leading-10 dark:text-Dark_TextColor_2">
             کارگاه های تخصصی و تکمیلی برای کار با پلتفرم های بازاری مورد اقبال و
             برگزاری تورنمت های تیمی رقابتی برای تقویت روحیه کار تیمی و آشنایی
             عمیق با ابزارهای مدیریت پروژه ، مدیریت سورس تیمی و دورکاری
@@ -64,13 +64,13 @@ export const Deatails = () => {
               src="./Image/Landing/Goals04.svg"
               alt="آموزش های تخصصی"
             />
-            <h3 className="mt-16 mr-4 text-xl font-light text-textColor">
+            <h3 className="mt-16 mr-4 text-xl font-light text-textColor dark:text-Dark_TextColor_1">
               آموزش های تخصصی
             </h3>
           </div>
         </div>
         <div className="h-36 mr-60" dir="ltr">
-          <p className="w-1/2 h-16 text-textColor leading-10">
+          <p className="w-1/2 h-16 text-textColor leading-10 dark:text-Dark_TextColor_2">
             جلسات تنظیم cv برای ساخت و اشتراک رزومه فنی در بسترهای داخلی و بین
             المللی کاریابی و آماده سازی دانش پژوهان برای شرکت در مصاحبه
           </p>
--- src/Components/Landing/HeroSection/Container.jsx
+++ src/Components/Landing/HeroSection/Container.jsx
@@ -4,8 +4,10 @@ function HeroSection() {
       className="h-[458px] mt-[30px] mx-auto w-[85%] text-right flex flex-col max-md:items-center md:justify-start"
       style={{ direction: "rtl" }}
     >
-      <h1 className="text-3xl text-textColor mt-16">آکادمی بحرالعلوم</h1>
-      <p className="max-md:px-8 inline-block text-textPcolor text-base md:w-[560px] leading-9 my-12">
+      <h1 className="text-3xl text-textColor mt-16 dark:text-Dark_TextColor_1">
+        آکادمی بحرالعلوم
+      </h1>
+      <p className="max-md:px-8 inline-block text-textPcolor text-base md:w-[560px] leading-9 my-12 dark:text-Dark_TextColor_2">
         آکادمی جامعه کوچکی است از ذهن های بزرگ برای رشد مراتب تخصص و دانستگی هدف
         تغییر سرنوشت هاست با درک دوباره لذت کدنویسی
       </p>
