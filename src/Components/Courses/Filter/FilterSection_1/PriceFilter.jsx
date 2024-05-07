2946b1ea711cb3375c6c918b9978d48ba28cd312
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-07 13:14:02 +0330

    price filter added

--- src/Components/Courses/Filter/FilterSection_1/PriceFilter.jsx
+++ src/Components/Courses/Filter/FilterSection_1/PriceFilter.jsx
@@ -1,26 +1,47 @@
-import { Slider } from "@mui/material";
-
-import * as React from "react";
+import { Box, Slider } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function PriceFilter() {
-  const [value, setValue] = React.useState([20, 37]);
+  const [value, setValue] = useState([30, 70]);
+
   const valuetext = (value) => {
     return `${value}°C`;
   };
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+
+  const minDistance = 10;
+  const handleChange = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue([Math.min(newValue[0], value[1] - minDistance), value[1]]);
+    } else {
+      setValue([value[0], Math.max(newValue[1], value[0] + minDistance)]);
+    }
   };
 
   return (
     <div className="flex justify-center">
-      {/* <Slider
-        getAriaLabel={() => "Temperature range"}
-        value={value}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        getAriaValueText={valuetext}
-      /> */}
-      PriceFilter
+      <span>قیمت :</span>
+
+      <Box className="w-[70%] h-[40px] mr-5 mt-4 relative">
+        <span className="absolute right-0 top-[-30px] text-[9px]">
+          حداکثر: ۴.۰۰۰.۰۰۰
+        </span>
+        <span className="absolute left-0 top-[-30px] text-[9px]">
+          حداقل: ۲۰۰.۰۰۰
+        </span>
+        <Slider
+          style={{ color: "#91ACCF", height: "6px", scale: "0.9" }}
+          getAriaLabel={() => "Minimum distance"}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          disableSwap
+        />
+      </Box>
     </div>
   );
 }
