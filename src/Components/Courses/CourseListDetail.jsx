e9fd1133a0284a837551f8874b3dd88b4b45d5d9
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-09 20:08:16 +0330

    some margin or padding updated

--- src/Components/Courses/CourseListDetail.jsx
+++ src/Components/Courses/CourseListDetail.jsx
@@ -14,23 +14,26 @@ export const CourseListDetail = ({
   // FaBeer,
 }) => {
   return (
-    <div className="w-[270px] p-2  h-auto flex-none shadow-3xl rounded-xl cursor-pointer">
-      <div className="h-44 bg-slate-500 rounded-xl">
+    <div className="w-[250px] p-[5px]  h-[405] flex-none shadow-3xl rounded-[11px] cursor-pointer">
+      <div className="w-[240px]  h-[147] bg-slate-500 rounded-[11px]">
         <img className="rounded-xl h-full w-full" src={courseImg} alt="image" />
       </div>
       <div className="h-auto rounded-xl">
         <h2 className="py-3 text-sm text-textColor dark:text-[#adb5bd]">
           {title}
         </h2>
-        <p className="text-textPaleColor text-xs pb-1 dark:text-[#868e96]">
+
+        <p className="text-textPaleColor mb-[10px] text-xs pb-1 dark:text-[#868e96]">
           {desc}
         </p>
+
         <div className="flex flex-row-reverse gap-2 items-center ml-2 mt-[-6px]">
           <div className="w-10 h-10 bg-slate-400 rounded-full">
             <img src={teacherImg} alt="teacher" />
           </div>
           <p className="text-xs text-textPaleColor">مدرس دوره</p>
         </div>
+
         <div className="flex items-center gap-2 mt-2 text-textSmallSize text-textMidColor tracking-tighter">
           <img className="w-5 h-5 my-1" src="./Image/Course/date.png" />
           <div>
@@ -43,6 +46,7 @@ export const CourseListDetail = ({
             <span> {startDate} </span>
           </div>
         </div>
+
         <div className="flex justify-between text-textSmallSize text-textMidColor mt-3 mb-2">
           <p>هزینه دوره</p>
           <div className="flex gap-1">
@@ -50,7 +54,9 @@ export const CourseListDetail = ({
             <img src="./Image/Course/toman.svg" alt="" />
           </div>
         </div>
+
         <div className="w-auto h-[1px] bg-gradient-to-r from-[#c4cdd512] via-[#c4cdd5] to-[#c4cdd512] "></div>
+
         <div className="flex justify-evenly pt-2">
           <div className="flex gap-1 items-center text-textMidColor">
             <span className="text-xs">{comment}</span>
