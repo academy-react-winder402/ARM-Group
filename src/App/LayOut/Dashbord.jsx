6b96755e7315a1c11745161adbcef90b6fac9fee
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-11 19:39:10 +0330

    margin top fixed

--- src/App/LayOut/Dashbord.jsx
+++ src/App/LayOut/Dashbord.jsx
@@ -4,12 +4,10 @@ import Pagination from "../../Components/Common/Pagination";
 
 function Dashbord({ children }) {
   return (
-    <div className="w-full h-[93.1vh]">
-      <div className="flex justify-between w-[1120px] h-[575px] m-auto mt-[57px] relative">
-        <Pagination />
-        {children}
-        <MenuBar />
-      </div>
+    <div className="mt-[100px] flex justify-between w-[1120px] h-[575px] m-auto relative">
+      <Pagination />
+      {children}
+      <MenuBar />
     </div>
   );
 }
