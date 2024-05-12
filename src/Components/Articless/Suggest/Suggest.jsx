3f971374daeecb40a45c6fe0bb24d0a38316a84f
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-12 09:43:59 +0330

    Article Some styling

--- src/Components/Articless/Suggest/Suggest.jsx
+++ src/Components/Articless/Suggest/Suggest.jsx
@@ -2,7 +2,7 @@ import { Header } from "./Header.jsx";
 import { Detail } from "./Detail.jsx";
 function Suggest({ title }) {
   return (
-    <div className="w-[350px] h-[590px] shadow-3xl rounded-[12px] p-4">
+    <div className="w-[350px] bg-white h-[590px] shadow-3xl rounded-[12px] p-4">
       <Header title={title} />
       <Detail />
       <Detail />
