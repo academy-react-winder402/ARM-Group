a8e5394f4a9e60df673ce8fd5d096788f9aa1f83
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-05-12 09:46:53 +0330

    Search Fixed

--- src/Components/Common/Search/index.jsx
+++ src/Components/Common/Search/index.jsx
@@ -22,7 +22,8 @@ function Index({ DeleteStatus }) {
         onClick={CheckCurrRadio}
         type="text"
         placeholder="دنبال چیز خاصی میگردی؟"
-        className="bg-transparent w-[90%] text-[13px] outline-0 "
+        className="bg-transparent w-[90%] text-[13px]"
+        style={{ outline: "none" }}
       />
 
       <svg
