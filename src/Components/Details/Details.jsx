6879c7bc2df6a9d4fed08e18dab44b6e4621f2f3
AmirrezaAshouri <amirrezaashoori3@gmail.com>
2024-05-13 23:36:10 +0330

    third commit

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -7,23 +7,23 @@ const Details = () => {
     <div className="w-[87.5%] mx-auto mt-[40px] pt-[1rem] bg-[#F3F3F3]" style={{direction: 'rtl'}}>
         <DetailsSection1 />
         <div className='h-[95px] pt-[10px] mt-[115px]'>
-            <div className='w-[45%] h-[100%] float-right'>
-                <h1 className='text-[24px] text-[#5E5E5E] font-bold'>خلاصه وضعیت دوره</h1>
-                <div className='mt-[15px]'>
-                    <p className='float-right text-[#7E7E7E] font-medium'>درصد انجام تمارین: ۷۸٪ </p>
+            <div className='w-[44%] h-[100%] float-right'>
+                <h1 className='mt-[-10px] text-[24px] text-[#5E5E5E] font-bold'>خلاصه وضعیت دوره</h1>
+                <div className='mt-[10px]'>
+                    <p className='float-right text-[#7E7E7E] font-medium xxx'>درصد انجام تمارین: ۷۸٪ </p>
                     <span className='w-[81px] h-[30px] text-center inline float-right mr-[1rem] rounded-[15px] border-[3px] border-[#DF9CD2] text-[#808080] text-[15px]'>متوسط</span>
                 </div>
             </div>
-            <div className='w-[55%] h-[100%] float-right text-[14px] text-[#6E6E6E] font-normal'>
-                <div className='flex flex-row flex-nowrap'>
-                    <p>تعداد روزهای دوره 230 روز</p>
-                    <div className='w-[231.5px] h-[4px] mt-[12px] rounded-[1rem] bg-[#DBDBDB] overflow-hidden'><div className='bg-[#69EECE] w-[50%] h-[100%] float-left'></div></div>
-                    <p>روزهای باقی مانده80روز</p>
+            <div className='w-[56%] h-[100%] float-right text-[14px] text-[#6E6E6E]'>
+                <div className='flex flex-row flex-nowrap mr-[100px]'>
+                    <div className='flex flex-row flex-nowrap'><span className=''>تعداد روز های دوره</span><span>۲۳۰</span><span>روز</span></div>
+                    <div className='w-[231.5px] h-[4px] mt-[8px] mr-[3px] rounded-[1rem] bg-[#DBDBDB] overflow-hidden'><div className='bg-[#69EECE] w-[50%] h-[100%] float-left'></div></div>
+                    <div className='flex flex-row flex-nowrap gap-[5px]'><span>روز‌های باقی مانده</span><span>۸۰</span><span>روز</span></div>
                 </div>
-                <div className='flex flex-row flex-nowrap mt-[15px]'>
-                    <p>شهریه دوره 4.200.000 تومان</p>
-                    <div className='w-[40%] h-[4px] mt-[12px] rounded-[1rem] bg-[#DBDBDB] overflow-hidden'><div className='bg-[#F3DD6D] w-[50%] h-[100%] float-left'></div></div>
-                    <p>باقی مانده شهریه 2.100.00 تومان</p>
+                <div className='flex flex-row flex-nowrap mt-[15px] mr-[100px]'>
+                    <div className='flex flex-row flex-nowrap'><span>شهریه دوره</span><span>۴.۲۰۰.۰۰۰</span><span>تومان</span></div>
+                    <div className='w-[231.5px] h-[4px] mt-[12px] mr-[3px] rounded-[1rem] bg-[#DBDBDB] overflow-hidden'><div className='bg-[#F3DD6D] w-[50%] h-[100%] float-left'></div></div>
+                    <div className='flex flex-row flex-nowrap'><span>باقی‌مانده شهریه</span><span>۲.۱۰۰.۰۰۰</span>تومان<span></span></div>
                 </div>
             </div>
         </div>
@@ -31,24 +31,24 @@ const Details = () => {
         <div className='flex flex-nowrap justify-between text-center pt-[35px] px-[65px]'>
             <div className='CourseDetailstexts'>
                 <div>۳۴۰</div>
-                <div className='text-[18px]'>تعداد دانشجو</div>
+                <div className='text-[18px] font-normal'>تعداد دانشجو</div>
             </div>
             <div className='CourseDetailstexts'>
                 <div>۱۴</div>
-                <div className='text-[18px]'>سرفصل آموزشی</div>
+                <div className='text-[18px] font-normal'>سرفصل آموزشی</div>
             </div>
             <div className='CourseDetailstexts'>
                 <div>۱۲</div>
-                <div className='text-[18px]'>جلسه فعلی</div>
+                <div className='text-[18px] font-normal'>جلسه فعلی</div>
             </div>
             <div className='CourseDetailstexts'>
                 <div>۱۲</div>
-                <div className='text-[18px]'>جلسات باقی مانده</div>
+                <div className='text-[18px] font-normal'>جلسات باقی مانده</div>
             </div>
         </div>
 
         <div className='mt-[45px] h-[500px] bg-[#FEFEFE] pt-[30px] rounded-[15px] '>
-            <div className='w-[95%] h-[60px] pt-[20px] mx-auto flex flex-nowrap justify-between rounded-[8px] px-[35px] bg-[#FFFFFF] drop-shadow-[-5px_5px_10px_rgba(0,0,0,0.11)]'>
+            <div className='w-[95%] h-[63px] pt-[17px] mx-auto flex flex-nowrap justify-between rounded-[8px] px-[35px] bg-[#FFFFFF] drop-shadow-[-5px_5px_10px_rgba(0,0,0,0.11)]'>
                 <div className='CourseDetailstexts2'>توضیحات</div>
                 <div className='CourseDetailstexts2'>سرفصل ها</div>
                 <div className='CourseDetailstexts2'>ارسال تسک</div>
