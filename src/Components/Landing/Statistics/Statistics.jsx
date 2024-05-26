4d825d178478289c3a57c72655b802d7b2107c33
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-05-25 19:36:45 -0700

    api call

--- src/Components/Landing/Statistics/Statistics.jsx
+++ src/Components/Landing/Statistics/Statistics.jsx
@@ -1,7 +1,27 @@
 import CountUp from "react-countup";
 import PN from "persian-number";
+import { LandingReportApi } from "../../../Core/Services/api/LandingReport";
+import { useEffect, useState } from "react";
 
 export const Statistics = () => {
+  const [techerCount, setTecherCount] = useState();
+  const [studentCount, setStudentCount] = useState();
+  const [courseCount, setCourseCount] = useState();
+  const [newsCount, setNewsCount] = useState();
+
+  const getReport = async () => {
+    const report = await LandingReportApi();
+    setTecherCount(report.teacherCount);
+    setStudentCount(report.studentCount);
+    setCourseCount(report.courseCount);
+    setNewsCount(report.newsCount);
+    console.log(report);
+  };
+
+  useEffect(() => {
+    getReport();
+  }, []);
+
   return (
     <div
       className="mt-[480px] h-[690px] md:w-[87.5%] mx-auto text-center"
@@ -16,20 +36,31 @@ export const Statistics = () => {
       </div>
       <div className="mt-[140px] flex flex-nowrap justify-center gap-[100px]">
         <div>
-          <span className="StNumbers dark:text-Dark_TextColor_1">۱۲۰۰</span>
+          <span className="StNumbers dark:text-Dark_TextColor_1">
+            <CountUp duration={5} end={studentCount} />
+            {/* {studentCount} */}
+          </span>
           <span className="StTexts dark:text-Dark_TextColor_1">دانشجو</span>
         </div>
         <div>
           <span className="StNumbers dark:text-Dark_TextColor_1 ">
-            <CountUp duration={5} end={78} />
+            {courseCount}
           </span>
           <span className="StTexts dark:text-Dark_TextColor_1">
             دوره آموزشی
           </span>
         </div>
         <div>
-          <span className="StNumbers dark:text-Dark_TextColor_1">۱۲</span>
+          <span className="StNumbers dark:text-Dark_TextColor_1">
+            <CountUp duration={5} end={techerCount} />
+          </span>
           <span className="StTexts dark:text-Dark_TextColor_1">استاد</span>
+        </div>
+        <div>
+          <span className="StNumbers dark:text-Dark_TextColor_1">
+            {newsCount}
+          </span>
+          <span className="StTexts dark:text-Dark_TextColor_1">اخبار</span>
         </div>
       </div>
     </div>
