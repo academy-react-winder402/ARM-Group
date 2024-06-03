f62b777266f4431fb88d2ed9bc12ac09d77dbbbb
AmirrezaAshouri <amirrezaashoori3@gmail.com>
2024-06-03 14:05:37 +0330

    commit by amirreza

--- src/Components/Landing/ScrolBtn/ScrolBtn.jsx
+++ src/Components/Landing/ScrolBtn/ScrolBtn.jsx
@@ -1,18 +1,21 @@
 const ScrolBtn = () => {
-  window.addEventListener("scroll", () => {
-    var scrollBtn = document.getElementById("scs");
-    if (window.pageYOffset > 200) {
-      scrollBtn.classList.remove("HideScrolBtn");
-      scrollBtn.classList.add("ShowScrolBtn");
-    } else {
-      scrollBtn.classList.remove("ShowScrolBtn");
-      scrollBtn.classList.add("HideScrolBtn");
+    window.addEventListener("scroll", () => {
+      try{
+      var scrollBtn = document.getElementById("scb");
+      if (window.pageYOffset > 200) {
+        scrollBtn.classList.remove("HideScrolBtn");
+        scrollBtn.classList.add("ShowScrolBtn");
+      } else {
+        scrollBtn.classList.remove("ShowScrolBtn");
+        scrollBtn.classList.add("HideScrolBtn");
+      }}
+      catch(e){}
     }
-  });
+    )
 
   return (
     <div
-      id="scs"
+      id="scb"
       className='HideScrolBtn fixed top-[550px] right-[2%] cursor-pointer w-[40px] h-[40px] rounded-full bg-[url("./Image/ScrolBtn/ScrolBtn.svg")] bg-[length:25px] bg-no-repeat bg-center border-orange-400 border-[2px]'
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
     ></div>
