40360444b1c7c417d1c2e2243d7f66e9fdd7af09
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-06-23 11:41:50 -0700

    last comment

--- src/Components/ArticleDetail/Center/Comments.jsx
+++ src/Components/ArticleDetail/Center/Comments.jsx
@@ -3,7 +3,7 @@ import { CommentDetail } from "./CommentDetail.jsx";
 function Comments() {
   return (
     <div>
-      <div className="h-[540px] sm:w-[700px] w-auto container relative translate-x-1/2 right-1/2 bg-white rounded-[15px] shadow-lg px-[14px] py-5 border mb-4 dark:bg-[#343a40]">
+      <div className="h-[540px] sm:w-[700px] w-auto container relative translate-x-1/2 right-1/2 bg-white rounded-[15px] shadow-lg px-[14px] py-5 mb-4 dark:bg-[#343a40]">
         <div className="flex gap-1 justify-between">
           <h3 className="text-xl text-textColor dark:text-[#dee2e6]">نظرات</h3>
           <div className="comments h-7 max-lg:text-[12px] max-sm:text-[10px] dark:text-[#ced4da]">
