c3fd633cccc7527204594ff62c6e12f64ea770c1
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-06-23 15:25:16 -0700

    fix courses view 2

--- src/Components/Courses/SecondView/RightSide.jsx
+++ src/Components/Courses/SecondView/RightSide.jsx
@@ -8,7 +8,10 @@ import { Scores } from "./Scores.jsx";
 
 function RightSide({ CourseObj }) {
   return (
-    <div className="w-1/2 max-lg:w-3/5 px-8">
+    <div
+      className="w-1/2 max-lg:w-3/5 px-8"
+      style={{ fontFamily: "IransnsNumber" }}
+    >
       <TopText
         title={CourseObj.title}
         desc={CourseObj.describe}
@@ -23,10 +26,9 @@ function RightSide({ CourseObj }) {
         // month={item.month}
       />
       <Scores
-        numberOfComment={CourseObj.likeCount}
-        numberOfScore={CourseObj.likeCount}
+        numberOfComment={CourseObj.commandCount}
         numberOfLike={CourseObj.likeCount}
-        numberOfUser={CourseObj.likeCount}
+        numberOfUser={CourseObj.currentRegistrants}
       />
     </div>
   );
